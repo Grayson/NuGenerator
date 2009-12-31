3a09c9ba4f4a9d06274bd07fd96dc3ada23e07dd
Grayson Hansard <info@fromconcentratesoftware.com>
2009-12-30 23:09:49 -0500

    Fixes a deprecation warning.

--- NuGenerator.m
+++ NuGenerator.m
@@ -7,7 +7,9 @@ int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
 	id parser = [Nu parser];
-	[parser parseEval:[NSString stringWithContentsOfFile:@"../../generator.nu"]];
+	
+	
+	[parser parseEval:[NSString stringWithContentsOfFile:@"../../generator.nu" encoding:NSUTF8StringEncoding error:nil]];
 
     [pool drain];
     return 0;
